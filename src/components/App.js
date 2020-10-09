8a152053ad46d2c49507c5744d4a5a39408a0064
Ivan <strokovhii@gmail.comgit config --global user.name Ivangit config --global user.email strokovhii@gmail.com>
2020-10-09 15:37:48 +0700

    Data upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,33 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react"
+import '../styles/style.scss'
 
-import '../styles/style.scss';
+function App() {
+  const URL = 'http://5d22b7fd4e05c600146ef4dd.mockapi.io/cupcake/books'
+  const [error, setError] = useState(null)
+  const [books, setBooks] = useState([])
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>My React App!</h1>
-            </div>
-        );
+  useEffect(() => {
+    if (!localStorage.getItem('books')) { 
+      fetch(URL, {method: 'GET', mode: 'cors', headers: {'Content-Type': 'application/json'}})
+        .then(response => response.json())
+        .then(response => {
+          setBooks(response.books)
+          return JSON.stringify(response.books)
+        })
+        .then(books => {
+          localStorage.setItem('books', books)
+        })
+        .catch(err => {
+          setError(err)
+        })
     }
+  }, [])
+
+  return (
+    <div>
+      <h1>My React App!</h1>
+    </div>
+  )
 }
 
-export default App;
+export default App
